e2f0d8f9eb4b954640287dc3820eabe601f73737
Sanne <sannevrijenhoek@gmail.com>
2018-11-16 11:24:37 +0100

    Elastic Connectors

---
+++ Elastic/Search.py
@@ -0,0 +1,84 @@
+import time
+from Elastic.Connector import Connector
+
+# Class dealing with all Elasticsearch Search operations. Contains the following queries:
+# - most popular documents
+# - random document
+# - similar document
+# - all documents in timerange
+
+
+class Search(Connector):
+    def __init__(self):
+        super(Connector, self).__init__()
+
+    def execute_search(self, index, body):
+        response = self.es.search(index=index, body=body)
+        return response['hits']['hits']
+
+    def get_most_popular(self, size):
+        body = {
+            "size": size,
+            "sort": [
+                {"popularity": {"order": "desc", "mode": "max", "unmapped_type": "long"}}
+            ],
+            "query": {
+                "match_all": {},
+            }}
+        return self.execute_search('articles', body)
+
+    def get_random_document(self, index):
+        timestamp = time.time()
+        body = {
+            "size": 1,
+            "query": {
+                  "function_score": {
+                     "functions": [
+                        {
+                           "random_score": {
+                              "seed": str(timestamp)
+                           }
+                        }
+                     ]
+                  }
+            }}
+        return self.execute_search(index, body)
+
+    def get_similar_documents(self, index, docid, size):
+        body = {
+            'size': size,
+            'query': {"more_like_this": {
+                "fields": ['text'],
+                "like": [
+                    {
+                        "_index": index,
+                        "_id": docid,
+                    },
+                ],
+        }}}
+        return self.execute_search(index, body)
+
+    def get_all_in_timerange(self, index, size, lower, upper):
+        body = {
+            'size': size,
+            "sort": [
+                {"popularity.facebook_share": {"order": "desc", "mode": "max", "unmapped_type": "long"}}
+            ],
+            'query': {"range": {
+                "publication_date": {
+                    "lt": upper,
+                    "gte": lower
+                }
+            }}}
+        return self.execute_search(index, body)
+
+    def get_all_documents(self, index, size, offset):
+        body = {
+            'size': size,
+            'from': offset,
+            "query": {
+                "match_all": {}
+            }
+        }
+        return self.execute_search(index, body)
+
---
+++ Elastic/TestRetrieve.py
@@ -0,0 +1,31 @@
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+
+es = Elasticsearch()
+
+client = Elasticsearch(host="localhost")
+
+s = Search(using=client, index="articles") \
+    .query("match", text="VVD")
+
+response = s.execute()
+
+print("Got %d Hits:" % response['hits']['total'])
+for hit in response:
+    print(hit.meta.score, hit.title)
+    mlts = es.search(index='articles', body={'query': {"more_like_this": {
+        "fields": ['text'],
+        "like": [
+            {
+                "_index": "articles",
+                "_id": hit.meta.id,
+            },
+        ],
+    }}})
+    for result in mlts['hits']['hits']:
+        try:
+            print("    "+result['_source']['publication_date'])
+            print("    Read:" + str(result['_source']['popularity']['no_read']))
+        except KeyError:
+            print("    Title unknown")
+
