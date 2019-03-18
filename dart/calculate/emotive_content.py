b423791a59323f7094188acc6df81e53545c3572
Sanne <sannevrijenhoek@gmail.com>
2019-03-18 14:16:33 +0100

    attempt build fix 1

--- dart/calculate/emotive_content.py
+++ dart/calculate/emotive_content.py
@@ -3,27 +3,33 @@ from dart.models.Article import Article
 import pandas as pd
 import spacy
 
-searcher = ArticleHandler()
-nlp = spacy.load('nl_core_news_sm', disable=['parser', 'ner'])
-
 # relativity_words = {'gebied', 'gaan', 'buigen'}
 # negation = ['niet', 'geen', 'nooit', 'niets', 'niemand', 'nergens']
 spacy_tags = ['DET', 'ADP', 'PRON']
 
-for x in range(0, 100):
-    document = Article(searcher.get_random_article())
-    print(document.title)
-    print(document.doctype)
-    doc = nlp(document.text)
-    tags = [token.pos_ for token in doc]
-    s = pd.Series(tags)
-    counts = s.value_counts()
 
-    for tag in spacy_tags:
-        try:
-            count = counts[tag]
-            percentage = count/len(s)
-            print('{}:\t{}'.format(tag, percentage))
-        except KeyError:
-            print('{}:\t{}'.format(tag, 0))
-    print()
+class IdentifyEmotiveContent:
+
+    def __init__(self):
+        self.searcher = ArticleHandler()
+        self.nlp = spacy.load('nl_core_news_sm', disable=['parser', 'ner'])
+
+    def execute(self):
+        for x in range(0, 100):
+            document = Article(self.searcher.get_random_article())
+            print(document.title)
+            print(document.doctype)
+            doc = self.nlp(document.text)
+            tags = [token.pos_ for token in doc]
+            s = pd.Series(tags)
+            counts = s.value_counts()
+
+            # TO DO: implement adding to Elasticsearch
+            for tag in spacy_tags:
+                try:
+                    count = counts[tag]
+                    percentage = count/len(s)
+                    print('{}:\t{}'.format(tag, percentage))
+                except KeyError:
+                    print('{}:\t{}'.format(tag, 0))
+            print()
