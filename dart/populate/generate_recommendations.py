f8e44c99c671ebf2f658aded29bbe3cd0b7d4a43
Sanne <sannevrijenhoek@gmail.com>
2019-03-20 13:26:06 +0100

    fix positional argument issue

--- dart/populate/generate_recommendations.py
+++ dart/populate/generate_recommendations.py
@@ -108,7 +108,7 @@ class RunRecommendations:
                     # select most popular
                     most_popular_recommendation = rg.generate_most_popular()
                     for docid in most_popular_recommendation:
-                        article = Article(self.searcher.get_by_id('articles', docid))
+                        article = Article(self.searcher.get_by_id(docid))
                         self.add_document(date, user_id, 'most_popular', article)
                     # get more like the user has previously read
                     more_like_this_recommendation = rg.generate_more_like_this(user, upper, lower)
