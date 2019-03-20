f931fb201ea95afce6c3cd418b633fca3a99cc38
Sanne <sannevrijenhoek@gmail.com>
2019-03-20 14:56:10 +0100

    trying another doctest

--- dart/calculate/emotive_content.py
+++ dart/calculate/emotive_content.py
@@ -3,33 +3,38 @@ from dart.models.Article import Article
 import pandas as pd
 import spacy
 
-# relativity_words = {'gebied', 'gaan', 'buigen'}
-# negation = ['niet', 'geen', 'nooit', 'niets', 'niemand', 'nergens']
-spacy_tags = ['DET', 'ADP', 'PRON']
-
 
 class IdentifyEmotiveContent:
 
     def __init__(self):
         self.searcher = ArticleHandler()
         self.nlp = spacy.load('nl_core_news_sm', disable=['parser', 'ner'])
+        self.spacy_tags = ['DET', 'ADP', 'PRON']
+
+    def calculate(self, s):
+        """
+        calculates for each tag specified its representation in the selected article
+        >>> IdentifyEmotiveContent.calculate(pd.Series({'text': 'Burgemeester', 'tag': 'NOUN'}, {'text': 'Rob', 'tag': 'NOUN'}, {'text': 'Bats', 'tag': 'PROPN'}))
+        {'DET': 0, 'ADP': 0, 'PROPN': 0.3333}
+        """
+        counts = s.value_counts()
+        result = {}
+        for tag in self.spacy_tags:
+            try:
+                count = counts[tag]
+                percentage = count / len(s)
+                result[tag] = percentage
+            except KeyError:
+                result[tag] = 0
+        return result
 
     def execute(self):
+        # TO DO: REWRITE FOR ALL RECOMMENDATIONS
         for _ in range(0, 100):
             document = Article(self.searcher.get_random_article())
-            print(document.title)
-            print(document.doctype)
-            doc = self.nlp(document.text)
-            tags = [token.pos_ for token in doc]
+            tags = document.tags
             s = pd.Series(tags)
-            counts = s.value_counts()
-
+            percentages = self.calculate(s)
             # TO DO: implement adding to Elasticsearch
-            for tag in spacy_tags:
-                try:
-                    count = counts[tag]
-                    percentage = count/len(s)
-                    print('{}:\t{}'.format(tag, percentage))
-                except KeyError:
-                    print('{}:\t{}'.format(tag, 0))
-            print()
+            print(percentages)
+
