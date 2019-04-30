4c0bb05369aeeabf7909810b0aa6d39a620f9849
Sanne <sannevrijenhoek@gmail.com>
2019-04-30 17:22:40 +0200

    added generating Facebook access token

--- dart/handler/other/facebook.py
+++ dart/handler/other/facebook.py
@@ -2,16 +2,28 @@ import urllib
 import json
 import logging
 import time
+import requests
 
 
 class RetrieveFacebook:
 
-    facebook_graph_url = 'https://graph.facebook.com/?id='
+    facebook_graph_url = 'https://graph.facebook.com/?fields=engagement{share_count}&id='
     module_logging = logging.getLogger('popularity')
+
+    def get_access_token(self):
+        url = 'https://graph.facebook.com/oauth/access_token'
+        payload = {
+            'grant_type': 'client_credentials',
+            'client_id': '******',
+            'client_secret': '*****'
+        }
+        response = requests.post(url, params=payload)
+        return response.json()['access_token']
 
     def make_request(self, url):
         try:
-            page = urllib.request.urlopen(self.facebook_graph_url + url)
+            full_url = self.facebook_graph_url + url+'&access_token='+self.get_access_token()
+            page = urllib.request.urlopen(full_url)
             content = json.loads(page.read())
         except urllib.error.HTTPError:
             self.module_logging.error("Graph API max reached")
@@ -21,6 +33,5 @@ class RetrieveFacebook:
 
     def get_facebook_info(self, url):
         content = self.make_request(url)
-        comments = content['share']['comment_count']
-        shares = content['share']['share_count']
-        return comments, shares
+        shares = content['engagement']['share_count']
+        return shares
