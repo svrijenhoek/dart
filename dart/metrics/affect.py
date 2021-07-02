64bb807f0423c05784eef7aa77f5d60dbc470262
Mateo Gutierrez Granada <mateo.gutierrez.granada@rtl.nl>
2021-07-02 14:30:27 +0200

    activation with discount

--- dart/metrics/affect.py
+++ dart/metrics/affect.py
@@ -12,16 +12,37 @@ class Affect:
         self.scores = {}
         self.language = config["language"]
 
-    def compute_polarity_distr(self, arr, bins_discretizer):
+    @staticmethod
+    def harmonic_number(n):
+        """Returns an approximate value of n-th harmonic number.
+
+        http://en.wikipedia.org/wiki/Harmonic_number
+        """
+        # Euler-Mascheroni constant
+        gamma = 0.57721566490153286060651209008240243104215933593992
+        return gamma + math.log(n) + 0.5 / n - 1. / (12 * n ** 2) + 1. / (120 * n ** 4)
+
+    def compute_polarity_distr(self, arr, bins_discretizer, adjusted=False):
         """ 
             Args:
             Return"
         """
         assert len(arr) > 0
+        n = len(arr)
+        sum_one_over_ranks = self.harmonic_number(n)
         arr_binned = bins_discretizer.transform(arr)
         distr = {}
-        for bin in list(range(bins_discretizer.n_bins)):
-            distr[bin] = round(np.count_nonzero(arr_binned == bin) / arr_binned.shape[0], 2) 
+        if adjusted:
+            for bin in list(range(bins_discretizer.n_bins)):
+                for indx, ele in enumerate(arr_binned[:,0]):
+                    if ele == bin:
+                        rank = indx + 1
+                        bin_freq = distr.get(bin, 0.)
+                        distr[bin] = bin_freq + 1 * 1 / rank / sum_one_over_ranks
+
+        else:
+            for bin in list(range(bins_discretizer.n_bins)):
+                distr[bin] = round(np.count_nonzero(arr_binned == bin) / arr_binned.shape[0], 2) 
         return distr
 
     @staticmethod
@@ -51,7 +72,7 @@ class Affect:
         arr_pool = np.array([abs(item.sentiment) for item in pool])
         bins_discretizer.fit(arr_pool)
         arr_recommendation = np.array([abs(item.sentiment) for item in recommendation])
-        distr_pool  = self.compute_polarity_distr(self, arr_pool, bins_discretizer)
-        distr_recommendation = self.compute_polarity_distr(self, arr_recommendation, bins_discretizer)
+        distr_pool  = self.compute_polarity_distr(self, arr_pool, bins_discretizer, adjusted=True)
+        distr_recommendation = self.compute_polarity_distr(self, arr_recommendation, bins_discretizer, adjusted=True)
         divergence = self.compute_kl_divergence(distr_pool, distr_recommendation)
         return divergence
